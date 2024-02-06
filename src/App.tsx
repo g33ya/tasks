14a5c77a7f398431b823bb532c17a49c8a725afb
g33ya <gscozzaro2004@gmail.com>
2024-02-06 12:56:31 -0500

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 Giovanna Scozzaro, UD CISC275 with React Hooks and TypeScript
             </header>
-            <p> Hello World</p>
+            <p>Hello World</p>
         </div>
     );
 }
