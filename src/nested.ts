7949252156e155dfb9e7b6110923bd35c16c81c1
g33ya <gscozzaro2004@gmail.com>
2024-02-27 16:50:04 -0500

    publishAll function

--- src/nested.ts
+++ src/nested.ts
@@ -152,7 +152,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    const published = questions.map(
+        (question: Question): Question => ({ ...question, published: true })
+    );
+    return published;
 }
 
 /***
