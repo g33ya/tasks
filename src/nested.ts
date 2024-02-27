74408a72c24256c657b8262af8bc4e46963c0c6b
g33ya <gscozzaro2004@gmail.com>
2024-02-27 17:07:48 -0500

    addNewQuestion function

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -116,15 +117,7 @@ export function toCSV(questions: Question[]): string {
     const csv = questions
         .map(
             (question: Question): string =>
-                question.id +
-                "," +
-                question.name +
-                "," +
-                question.options.length +
-                "," +
-                question.points +
-                "," +
-                question.published
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
         )
         .join("\n");
     return csv;
@@ -177,7 +170,8 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const blankQuestion = makeBlankQuestion(id, name, type);
+    return [...questions, blankQuestion];
 }
 
 /***
