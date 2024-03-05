813b7e6a1752066f809639a86dda05251c0dfbd4
g33ya <gscozzaro2004@gmail.com>
2024-03-05 01:05:00 -0500

    startattempt button funcrions

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -5,5 +5,20 @@ export function StartAttempt(): JSX.Element {
     const [attempt, setAttempt] = useState<number>(4);
     const [progress, setProgress] = useState<boolean>(false);
 
-    return <div>Start Attempt</div>;
+    function startQuiz(): void {
+        if (attempt > 0) {
+            setProgress(true);
+            setAttempt(attempt - 1);
+        }
+    }
+
+    function stopQuiz(): void {
+        setProgress(false);
+    }
+
+    function Mulligan(): void {
+        setAttempt(1 + attempt);
+    }
+
+    return <span></span>;
 }
