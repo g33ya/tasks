4c80ae7a2c24fe3808730ea029131b8afe65af4d
g33ya <gscozzaro2004@gmail.com>
2024-03-05 00:56:02 -0500

    startattempt states

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,8 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
+    const [attempt, setAttempt] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+
     return <div>Start Attempt</div>;
 }
