8074f1d7ef7d05b7d9951c0d3386b45280646006
g33ya <gscozzaro2004@gmail.com>
2024-03-05 19:12:01 -0500

    alphabetical and by date functions, cycleholiday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,42 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
+    type holiday =
+        | "Fourth of July"
+        | "Halloween"
+        | "Christmas"
+        | "Thanksgiving"
+        | "Easter";
+
+    const [holiday, setHoliday] = useState<holiday>("Fourth of July");
+
+    function holidaysAlphabetized(): void {
+        if (holiday === "Fourth of July") {
+            setHoliday("Halloween");
+        } else if (holiday === "Halloween") {
+            setHoliday("Thanksgiving");
+        } else if (holiday === "Thanksgiving") {
+            setHoliday("Christmas");
+        } else if (holiday === "Christmas") {
+            setHoliday("Easter");
+        } else if (holiday === "Easter") {
+            setHoliday("Fourth of July");
+        }
+    }
+
+    function byDate(): void {
+        if (holiday === "Fourth of July") {
+            setHoliday("Halloween");
+        } else if (holiday === "Halloween") {
+            setHoliday("Thanksgiving");
+        } else if (holiday === "Thanksgiving") {
+            setHoliday("Christmas");
+        } else if (holiday === "Christmas") {
+            setHoliday("Easter");
+        } else if (holiday === "Easter") {
+            setHoliday("Fourth of July");
+        }
+    }
+
     return <div>Cycle Holiday</div>;
 }
