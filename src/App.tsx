937794d5eb3a84a0715140eaaf28061b33d2715e
g33ya <gscozzaro2004@gmail.com>
2024-03-05 20:00:00 -0500

    added back original website elements, finished task 8!

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
+import catpic from "./images/cat.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -14,18 +16,54 @@ function App(): JSX.Element {
             <header className="App-header">
                 Giovanna Scozzaro, UD CISC275 with React Hooks and TypeScript
             </header>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            <div>
+                <hr></hr>
+                <Counter></Counter>
+                <hr />
+                <RevealAnswer></RevealAnswer>
+                <hr />
+                <StartAttempt></StartAttempt>
+                <hr />
+                <TwoDice></TwoDice>
+                <hr />
+                <ChangeType></ChangeType>
+                <hr />
+                <CycleHoliday></CycleHoliday>
+            </div>
+            <h3>Time to pass these tests!</h3>
+            <p>Hello World</p>
+            <img src={catpic} alt="A COOL cat" width="30%" height="30%" />
+            <p>To-do List:</p>
+            <ul>
+                <li>Buy a cool pair of sunglasses</li>
+                <li>Ponder... for a while..</li>
+                <li>Eliminate all ants</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <p></p>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="Rectangles">
+                            <p>Oh my God!! I'm in a rectangle!!!</p>
+                        </div>
+                        <p>
+                            let me in.. it's lonely out here in the left column
+                        </p>
+                    </Col>
+                    <Col>
+                        <div className="Rectangles">
+                            <p>Yoooo me too! Ugly color fr.</p>
+                        </div>
+                        <p>
+                            sucks 4 u leftie. right column ON TOP! (but below
+                            the rectangle?? k.)
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
