0d75fc881f90cb6fcd8fbe4aabcc18acf43136ba
g33ya <gscozzaro2004@gmail.com>
2024-03-07 19:36:07 -0500

    created states for editmode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 
 export function EditMode(): JSX.Element {
+    const [isHappy, setIsHappy] = useState<boolean>(true);
+
+    // This is the Control
+    function updateHappiness(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsHappy(event.target.checked);
+    }
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="is-happy-check"
+                label="Happy?"
+                checked={isHappy}
+                onChange={updateHappiness}
+            />
+            <div>The user is {isHappy ? "happy" : "not happy"}.</div>
         </div>
     );
 }
