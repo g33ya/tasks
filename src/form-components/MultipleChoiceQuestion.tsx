08836620e709424c8ce2d048841fd4e857779091
g33ya <gscozzaro2004@gmail.com>
2024-03-07 20:15:31 -0500

    created state for multiplechoice

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -8,23 +9,22 @@ export function MultipleChoiceQuestion({
     expectedAnswer: string;
 }): JSX.Element {
     // This is the State (Model)
-    const [emotion, setEmotion] = useState<string>("happy");
-
+    const [currentChoice, setCurrentChoice] = useState<string>(options[0]);
     // This is the Control
-    function updateEmotion(event: React.ChangeEvent<HTMLSelectElement>) {
-        setEmotion(event.target.value);
-        return (
-            <div>
-                <Form.Group controlId="userEmotions">
-                    <Form.Label>How do you feel?</Form.Label>
-                    <Form.Select value={emotion} onChange={updateEmotion}>
-                        <option value="happy">Happy</option>
-                        <option value="sad">Sad</option>
-                        <option value="angry">Angry</option>
-                    </Form.Select>
-                </Form.Group>
-                The user is feeling {emotion}.
-            </div>
-        );
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrentChoice(event.target.value);
     }
+    return (
+        <div>
+            <Form.Group controlId="multipleChoice">
+                <Form.Label>How do you feel?</Form.Label>
+                <Form.Select value={currentChoice} onChange={updateChoice}>
+                    <option value="happy">Happy</option>
+                    <option value="sad">Sad</option>
+                    <option value="angry">Angry</option>
+                </Form.Select>
+            </Form.Group>
+            The user is feeling {currentChoice}.
+        </div>
+    );
 }
