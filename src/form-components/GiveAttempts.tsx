04d93f3ea8ba4c35616a2c75e8cbe7b7e4aaa5b3
g33ya <gscozzaro2004@gmail.com>
2024-03-07 19:16:11 -0500

    giveAttempt progress (text box for numbers)

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptRequest, setAttemptRequest] = useState<string>("");
+    const attemptRequest = parseInt(attemptRequest) || 0;
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>
+                <Form.Group controlId="attemptRequest">
+                    <Form.Label>Requested number of attempts:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        value={attemptRequest}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setAttemptRequest(attemptRequest)}
+                    />
+                </Form.Group>
+            </div>
         </div>
     );
 }
